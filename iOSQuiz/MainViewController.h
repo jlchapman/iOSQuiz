dd5c893bdefeb280e9a4973364f8549052a7976b
jlchapman <jlchapman@server.local>
2014-05-10 18:16:48 -0600

    Added some more code

---
+++ iOSQuiz/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  iOSQuiz
+//
+//  Created by Jerry Chapman on 5/10/14.
+//  Copyright (c) 2014 Jerry Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *showName;
+@property (weak, nonatomic) IBOutlet UILabel *showAnswer;
+
+@end
